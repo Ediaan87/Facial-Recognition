6a3b39af87b5fb5886a05017bf4fe2a7e3cecc75
1amth3l1ght <139058879+Ediaan87@users.noreply.github.com>
2023-10-01 15:54:49 +0200

    Added functionality to the createSearch action to scrape the web for similar images.

--- src/server/actions.js
+++ src/server/actions.js
@@ -1,4 +1,5 @@
 import HttpError from '@wasp/core/HttpError.js'
+import axios from 'axios'
 
 export const createSearch = async (args, context) => {
   if (!context.user) { throw new HttpError(401) };
@@ -13,6 +14,16 @@ export const createSearch = async (args, context) => {
       user: { connect: { id: user.id } }
     }
   });
+
+  const similarImages = await scrapeWebForSimilarImages(args.imageUrl)
+  for (const imageUrl of similarImages) {
+    await context.entities.Result.create({
+      data: {
+        imageUrl,
+        search: { connect: { id: search.id } }
+      }
+    });
+  }
 
   return search;
 }
@@ -32,4 +43,13 @@ export const addResult = async (args, context) => {
       search: { connect: { id: args.searchId } }
     }
   });
+}
+
+const scrapeWebForSimilarImages = async (imageUrl) => {
+  // TODO: Replace this with a call to the reverse image search API
+  // const response = await axios.get('https://api.example.com/reverse-image-search', {
+  //   params: { imageUrl }
+  // });
+  // return response.data.similarImages;
+  return [];
 }
